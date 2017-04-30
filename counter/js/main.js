0895e0140c82f57af1463f15d15fa2bffee3f54f
toussam87 <toussaint.am@gmail.com>
2017-04-30 19:28:12 -0400

    4/30/17

--- counter/js/main.js
+++ counter/js/main.js
@@ -1,4 +1,5 @@
 function decreaseCount() {
+<<<<<<< HEAD
     var currentRowCount = document.getElementById("rowCount").innerHTML;
     if (currentRowCount > 0) {
         var decrease = currentRowCount--;
@@ -15,3 +16,34 @@ window.onload = function decreaseFunction() {
     var rowCount = document.getElementById("decrease");
     rowCount.addEventListener("click", decreaseCount, false);
 }
+=======
+    var currentRowCountNumber = parseInt(document.getElementById("rowCount").innerHTML);
+if (currentRowCountNumber > 0) {
+    currentRowCountNumber--;
+    document.getElementById("rowCount").innerHTML = currentRowCountNumber;
+} else {
+    alert("You currently have zero rows.")
+    }
+};
+
+function increaseCount() {
+    var currentRowCountNumber = parseInt(document.getElementById("rowCount").innerHTML);
+    currentRowCountNumber++;
+    document.getElementById("rowCount").innerHTML = currentRowCountNumber;
+};
+
+window.onload = function() {
+    decreaseFunction();
+    increaseFunction();
+};
+
+function decreaseFunction() {
+var decreaseRow = document.getElementById("decrease");
+decreaseRow.addEventListener("click", decreaseCount, false);
+};
+
+function increaseFunction() {
+var increaseRow = document.getElementById("increase");
+increaseRow.addEventListener("click", increaseCount, false);
+};
+>>>>>>> ugh
