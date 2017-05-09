d4f2f9cea844075c29fb6926ddf2129e4a2be67a
Toussaint, Anne-Michelle <AToussaint@victoria.com>
2017-05-09 16:03:52 -0400

    050917 added structure and logic to max row count field

--- counter/js/main.js
+++ counter/js/main.js
@@ -14,17 +14,35 @@ function increaseCount() {
     document.getElementById("rowCount").innerHTML = currentRowCountNumber;
 };
 
+function changeMaxRowCount(event) {
+    var a = event.target.value;
+
+    if (isNaN(a) === false) {
+        if (a > 0) {
+            alert("it is bigger than 0");
+        } 
+        else {
+                alert("You've entered a number less than or equal to zero.");
+        }
+    }
+    else {
+        alert("Please enter a number.");
+    }
+    debugger;
+};
+
 window.onload = function() {
     decreaseFunction();
     increaseFunction();
+    document.getElementById("maxRowCount").addEventListener("change", changeMaxRowCount, false);
 };
 
 function decreaseFunction() {
-var decreaseRow = document.getElementById("decrease");
-decreaseRow.addEventListener("click", decreaseCount, false);
+    var decreaseRow = document.getElementById("decrease");
+    decreaseRow.addEventListener("click", decreaseCount, false);
 };
 
 function increaseFunction() {
-var increaseRow = document.getElementById("increase");
-increaseRow.addEventListener("click", increaseCount, false);
+    var increaseRow = document.getElementById("increase");
+    increaseRow.addEventListener("click", increaseCount, false);
 };
