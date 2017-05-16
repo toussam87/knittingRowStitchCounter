33013faca8d7fed782839190303c366a134edc5a
toussam87 <toussaint.am@gmail.com>
2017-05-16 16:37:07 -0400

    Add files via upload
    051617 cleaned up js so when DOM changes, updateRowCounterDisplay() updates the dom. Current having issues with acceptance requirement #3

--- counter/js/main.js
+++ counter/js/main.js
@@ -1,52 +1,65 @@
 var maxRowCountNumber;
+var currentRowCountNumber = 0;
 
 //function to decrease current Row Count
 function decreaseCount() {
-    var currentRowCountNumber = parseInt(document.getElementById("rowCount").innerHTML);
-if (currentRowCountNumber > 0) {
-    currentRowCountNumber--;
-    document.getElementById("rowCount").innerHTML = currentRowCountNumber;
-} else {
-    alert("You currently have zero rows.")
+    if (currentRowCountNumber > 0) {
+        currentRowCountNumber--;
+    } else {
+        alert("You currently have zero rows.")
     }
+    updateRowCounterDisplay();
 };
 
 //function to increase current Row Count
 function increaseCount() {
-    var currentRowCountNumber = parseInt(document.getElementById("rowCount").innerHTML);
-    currentRowCountNumber++;
     //checks to see if current Row Count is less than or equal to the desired set number of max row count
-    if (currentRowCountNumber < maxRowCountNumber) {
-        document.getElementById("rowCount").innerHTML = currentRowCountNumber;
+    if (currentRowCountNumber !== maxRowCountNumber) {
         currentRowCountNumber++;
+        updateRowCounterDisplay();
     }
 
     //checks to see if current Row Count is equal to the desired set number of max row count
     else if (currentRowCountNumber === maxRowCountNumber) {
-        document.getElementById("rowCount").innerHTML = currentRowCountNumber;
         alert("You have reached your desired number of rows.");
         currentRowCountNumber++;
+         updateRowCounterDisplay();
     } 
     //increments current row count past set max row count
     else {
-        document.getElementById("rowCount").innerHTML = currentRowCountNumber;
         alert("You are going over your desired number of rows.");
         currentRowCountNumber++;
+        updateRowCounterDisplay();
+    };
+};
+
+function updateRowCounterDisplay() {
+
+    // maxRowCountNumber
+    // currentRowCountNumber
+    var rowCounterDisplay = document.getElementById("rowCount");
+
+    if (maxRowCountNumber > 0) {
+        rowCounterDisplay.innerHTML = currentRowCountNumber + " of " + maxRowCountNumber;
+    } 
+    else {
+        rowCounterDisplay.innerHTML = currentRowCountNumber;
     };
 };
 
 // function sets max Row count
 function changeMaxRowCount(event) {
     var a = event.target.value;
-    a.id = "maxRowCountNumber";
+
     console.log(document.getElementById(maxRowCountNumber));
 
     if (isNaN(a) === false) {
         if (a > 0) {
-            maxRowCountNumber = a;
+            maxRowCountNumber = parseInt(a);
+            updateRowCounterDisplay();
         } 
         else {
-                alert("You've entered a number less than or equal to zero.");
+            alert("You've entered a number less than or equal to zero.");
         }
     }
     else {
